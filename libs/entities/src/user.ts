e0f1afc7d94032d7a6266aaab11831cac5cd827b
kerimCetinbas <a.kerim.cetinbas@yandex.com>
2022-09-08 00:59:30 +0300

    typegoose
    install actual package of typegoose

--- libs/entities/src/user.ts
+++ libs/entities/src/user.ts
@@ -1,32 +1,43 @@
-import { pre, prop, Ref } from "typegoose";
-
+import { DocumentType, pre, prop, Ref} from "@typegoose/typegoose";
+import {} from "typegoose"
+import bcrypt from "bcrypt"
 import Note from "./notes";
-bcrypt.genSalt(saltRounds, function(err, salt) {
-    bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
-        // Store hash in your password DB.
-    });
-});
+import { BeAnObject, IObjectWithTypegooseFunction , ReturnModelType } from "@typegoose/typegoose/lib/types";
 
 
-@pre<User>('save', function() {
-    this.isKitten = this.age < 1
+
+
+@pre<User>('save', async function() {
+    const hash = await bcrypt.hash(this.password, await bcrypt.genSalt(10));
+    this.password = hash
 })
-    
     
 class User {
     @prop({ required: true })
-    public name?: string;
+    public name!: string;
 
     @prop({ required: true })
-    public password?: string;
+    public password!: string;
 
     @prop({ required: true })
-    public email?: string;
+    public email!: string;
 
     @prop({ref: () => Note })
     public notes?: Ref<Note>[]
 
+    public static async comparePasswrod(this: ReturnModelType<typeof User>, user: { password: string, email: string, name: string }) {
 
+        const _user = await this.findOne(user).exec();
+
+        if (_user) {
+
+            const isPasswordCorrect = bcrypt.compareSync(user.password, _user.password);
+            return isPasswordCorrect;
+        }
+        
+        return false;
+
+    }
 
 
 
